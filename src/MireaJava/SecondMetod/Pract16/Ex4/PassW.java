f03e761c0acfc0bdf06e8da9995b5b2a05b4035a
AngelDuzt <st.ivan6901@gmai.com>
2022-12-15 12:20:38 +0300

    Мирэа Практики 2 курс 1 сем

--- src/MireaJava/SecondMetod/Pract16/Ex4/PassW.java
+++ src/MireaJava/SecondMetod/Pract16/Ex4/PassW.java
@@ -2,11 +2,26 @@ package MireaJava.SecondMetod.Pract16.Ex4;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Objects;
 
-public class PassW extends JFrame {
-    public static void createPanelUI(Container container) {
+public class PassW extends JFrame implements KeyListener {
+
+    public PassW(String title) throws HeadlessException {
+        super(title);
+    }
+
+    private final JTextField serviceField = new JTextField(30);
+    private final JTextField userName = new JTextField(30);
+    private final JTextField passwordField = new JTextField(30);
+    private final String password = "mypassword";
+
+    public void createPanelUI(Container container) {
 
         container.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+
+        passwordField.addKeyListener(this);
 
         container.setLayout(new GridBagLayout());
         GridBagConstraints constraints = new GridBagConstraints();
@@ -32,21 +47,18 @@ public class PassW extends JFrame {
         constraints.gridy = 2;
         container.add(passwordLabel, constraints);
 
-        JTextField serviceField = new JTextField(30);
         constraints.weightx = 0.0;
         constraints.gridwidth = 2;
         constraints.gridx = 1;
         constraints.gridy = 0;
         container.add(serviceField, constraints);
 
-        JTextField userName = new JTextField(30);
         constraints.weightx = 0.0;
         constraints.gridwidth = 2;
         constraints.gridx = 1;
         constraints.gridy = 1;
         container.add(userName, constraints);
 
-        JTextField passwordField = new JTextField(30);
         constraints.weightx = 0.0;
         constraints.gridwidth = 2;
         constraints.gridx = 1;
@@ -56,12 +68,45 @@ public class PassW extends JFrame {
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Password application");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        PassW obj = new PassW("");
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.createPanelUI(obj.getContentPane());
 
-        createPanelUI(frame.getContentPane());
+        obj.pack();
+        obj.setVisible(true);
+    }
 
-        frame.pack();
-        frame.setVisible(true);
+    @Override
+    public void keyTyped(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            if (Objects.equals(passwordField.getText(), password)) {
+                JOptionPane.showMessageDialog(null, "Right password", "Window", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, "Wrong password", "Window", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            if (Objects.equals(passwordField.getText(), password)) {
+                JOptionPane.showMessageDialog(null, "Right password", "Window", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, "Wrong password", "Window", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            if (Objects.equals(passwordField.getText(), password)) {
+                JOptionPane.showMessageDialog(null, "Right password", "Window", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, "Wrong password", "Window", JOptionPane.ERROR_MESSAGE);
+            }
+        }
     }
 }
+
